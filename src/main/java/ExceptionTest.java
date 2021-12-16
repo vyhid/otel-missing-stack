988aa427a1163afc098cfc6a3b40ba0740633713
Andrii Vyhidnyi <andrii.vyhidnyi@motorolasolutions.com>
2021-12-16 13:47:12 +0200

    Initial commit

---
+++ src/main/java/ExceptionTest.java
@@ -0,0 +1,26 @@
+import io.opentelemetry.extension.annotations.WithSpan;
+import org.springframework.web.reactive.function.client.WebClient;
+
+public class ExceptionTest {
+    //VM args
+    //-javaagent:"C:\Users/avyhidn/Downloads/opentelemetry-javaagent.jar"
+    //-Dotel.resource.attributes=service.name="my-app"
+    //-Dotel.javaagent.debug=true
+
+    // Evn
+    // OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:8200
+    public static void main(String[] args) {
+        testSpan();
+    }
+
+    @WithSpan("Span test")
+    private static void testSpan() {
+        Object block = WebClient.create("http://localhost:8090")
+                .get()
+                .exchange()
+                .block();
+
+        System.out.println(block);
+    }
+
+}
